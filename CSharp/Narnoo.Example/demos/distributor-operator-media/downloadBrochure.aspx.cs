e4f09b3753b26c259de6e937e1e96baadaa54751
cnlangzi <support@reakoo.com>
2012-04-30 09:05:42 +0800

    Finished demos/distributor-operator-media/downBrochure

---
+++ CSharp/Narnoo.Example/demos/distributor-operator-media/downloadBrochure.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Narnoo.Example.demos.distributor_operator_media
+{
+    public partial class downloadBrochure : DistributorPageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.lblMessage.Visible = false;
+        }
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            var operator_id = this.txtoperator_id.Text;
+            string brochure_id = this.txtBrochure_id.Text;
+
+            try
+            {
+                var request = new DistributorOperatorMediaNarnooRequest();
+                request.SetAuth(this.appkey, this.secretkey);
+                var item = request.DownloadBrochure(operator_id,brochure_id);
+
+                if (item == null)
+                {
+                    this.lblMessage.Visible = true;
+                    this.lblMessage.Text = "Brochure cannot found";
+                }
+                else
+                {
+
+                    this.lblDownload_brochure_to_pdf_path.Text = item.download_brochure_to_pdf_path;
+
+                }
+
+            }
+            catch (InvalidNarnooRequestException ex)
+            {
+                this.lblMessage.Visible = true;
+                this.lblMessage.Text = "ErrorCode:" + ex.Error.ErrorCode + "</br> ErrorMessage:" + ex.Error.ErrorMessage;
+            }
+        }
+    }
+}
--- CSharp/Narnoo/DistributorOperatorMediaNarnooRequest.cs
+++ CSharp/Narnoo/DistributorOperatorMediaNarnooRequest.cs
@@ -6,5 +6,25 @@ namespace Narnoo
 {
     public class DistributorOperatorMediaNarnooRequest : NarnooRequest
     {
+
+        string remote_url = "devapi.narnoo.com/xml.php"; // Distributor -> Operator
+        public DownloadBrochure DownloadBrochure(string operator_id,string brochure_id)
+        {
+            var content = this.GetResponse(this.remote_url, "downloadBrochure", 
+                new RequestParameter("operator_id",operator_id),
+                new RequestParameter("brochure__id", brochure_id));
+
+
+            var list = this.Deserialize<DownloadBrochuresResponse>(content);
+
+            if (list != null && list.download_brochure.Count > 0)
+            {
+                return list.download_brochure[0].download_brochure_details;
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
